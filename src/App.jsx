ebb5764cfa80090e70c8f4cb63dd4cae0bbf10a5
Emilia Bellido <emiliab200424@gmail.com>
2026-08-29 22:23:35 -0700

    Basic skeletonfor profile and swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 import { useState } from 'react'
 import './App.css'
-import NavBar from './components/Navbar'
+import NavBar from './components/Navbar';
+import ProjectSwiper from './components/ProjectSwiper';
+import MyProfile from './components/MyProfile';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
 
   return (
-    <NavBar />
-  )
-}
+    <>
+      <NavBar />
+      <div className = "container">
+        <div className = "row">
+          <div className = "col-md-6">
+            <MyProfile />
+          </div>
+          <div className = "col-md-6">
+            <ProjectSwiper />
+          </div>
+        </div>
+        
+        
+
+      </div>
+      
+    
+    </>
+    
+  );
+};
 
 export default App;
