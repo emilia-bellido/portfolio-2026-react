0c2879f2d94c260c96b272ebb82b257be0429d82
Emilia Bellido <emiliab200424@gmail.com>
2026-08-29 23:21:11 -0700

    Wired up Contact Form to be able to recieve emails

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export default function ContactForm() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", import.meta.env.VITE_WEB3FORMS_KEY);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    setResult(data.success ? "Success!" : "Error");
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Control name="botcheck" type="checkbox" style={{ display: 'none' }}/>
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control name="name" type="text" placeholder="Enter your Name" />
+      </Form.Group>
+      
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control name="email" type="email" placeholder="Enter your Email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formText">
+        <Form.Label>Message</Form.Label>
+        <Form.Control name="message" as="textarea" 
+      rows={4} placeholder="Message" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      <p>{result}</p>
+    </Form>
+  );
+}
