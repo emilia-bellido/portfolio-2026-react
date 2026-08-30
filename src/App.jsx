22848bcaac130f93a36c1bd2e67d19fb5dd3fd93
Emilia Bellido <emiliab200424@gmail.com>
2026-08-30 14:37:42 -0700

    Added an AllWorks Section with fucntional API Call

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import NavBar from './components/Navbar';
 import ProjectSwiper from './components/ProjectSwiper';
 import MyProfile from './components/MyProfile';
 import ContactForm from './components/ContactForm';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ProjectCard from './components/ProjectCard';
+import AllWorkSection from './components/AllWorkSection';
 
 
 function App() {
-
+  
   return (
     <>
       <NavBar />
 
-      <div className = "container">
+      <section className = "container" id="about">
         <div className = "row">
           <div className = "col-md-6">
             <MyProfile />
@@ -22,8 +25,13 @@ function App() {
             <ProjectSwiper />
           </div>
         </div>   
+      </section>
 
-      </div>
+     <section id="all-works" className="container py-5">
+        <h2 className="mb-4">All Works</h2>
+        <AllWorkSection />
+      </section>
+
       <ContactForm />
       
     
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,22 @@
+import Card from 'react-bootstrap/Card';
+//import { Link } from 'react-router-dom';
+
+const ProjectCard = ({id, title, image}) => {
+    return (
+        //Makes the Card clickable : if its the hotel-dashboard, the link will redirect to the pages project
+       // <Link to={`/projects/${id}`}>
+       <>
+            <Card>
+                <Card.Img variant="top" src= {image} />
+                <Card.Body>
+                <Card.Text>
+                   {title}
+                </Card.Text>
+                </Card.Body>
+            </Card>
+        </>
+       // </Link>
+    )
+}
+
+export default ProjectCard;
